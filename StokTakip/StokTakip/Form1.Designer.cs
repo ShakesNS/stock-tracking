f2f8e4152bba2f5cbda9c154dbe672e23fa04b27
Enes ÇAPKIN <m.enes.capkin@gmail.com>
2023-06-19 15:15:46 +0300

    Add files via upload

---
+++ StokTakip/StokTakip/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace StokTakip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbKategoriListele = new System.Windows.Forms.ComboBox();
+            this.lwListeleme = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbUrunAdi = new System.Windows.Forms.TextBox();
+            this.tbBirimFiyat = new System.Windows.Forms.TextBox();
+            this.cbKategori = new System.Windows.Forms.ComboBox();
+            this.cbMarketAdi = new System.Windows.Forms.ComboBox();
+            this.dtTarih = new System.Windows.Forms.DateTimePicker();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbMarketListeleme = new System.Windows.Forms.ComboBox();
+            this.contextBtn = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.güncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextBtn.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbKategoriListele
+            // 
+            this.cbKategoriListele.FormattingEnabled = true;
+            this.cbKategoriListele.Location = new System.Drawing.Point(115, 46);
+            this.cbKategoriListele.Name = "cbKategoriListele";
+            this.cbKategoriListele.Size = new System.Drawing.Size(182, 21);
+            this.cbKategoriListele.TabIndex = 0;
+            this.cbKategoriListele.Text = "Bİr Değer Seçiniz";
+            this.cbKategoriListele.SelectionChangeCommitted += new System.EventHandler(this.cbKategoriListele_SelectionChangeCommitted);
+            // 
+            // lwListeleme
+            // 
+            this.lwListeleme.ContextMenuStrip = this.contextBtn;
+            this.lwListeleme.FullRowSelect = true;
+            this.lwListeleme.GridLines = true;
+            this.lwListeleme.HideSelection = false;
+            this.lwListeleme.Location = new System.Drawing.Point(13, 79);
+            this.lwListeleme.Name = "lwListeleme";
+            this.lwListeleme.Size = new System.Drawing.Size(594, 439);
+            this.lwListeleme.TabIndex = 1;
+            this.lwListeleme.UseCompatibleStateImageBehavior = false;
+            this.lwListeleme.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(9, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kategoriler";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(630, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kategori:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(630, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ürün Adı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(613, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Birim Fiyatı:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(654, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Tarih:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(613, 235);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Market Adı:";
+            // 
+            // tbUrunAdi
+            // 
+            this.tbUrunAdi.Location = new System.Drawing.Point(711, 115);
+            this.tbUrunAdi.Name = "tbUrunAdi";
+            this.tbUrunAdi.Size = new System.Drawing.Size(153, 20);
+            this.tbUrunAdi.TabIndex = 9;
+            // 
+            // tbBirimFiyat
+            // 
+            this.tbBirimFiyat.Location = new System.Drawing.Point(711, 155);
+            this.tbBirimFiyat.Name = "tbBirimFiyat";
+            this.tbBirimFiyat.Size = new System.Drawing.Size(153, 20);
+            this.tbBirimFiyat.TabIndex = 10;
+            // 
+            // cbKategori
+            // 
+            this.cbKategori.FormattingEnabled = true;
+            this.cbKategori.Location = new System.Drawing.Point(711, 75);
+            this.cbKategori.Name = "cbKategori";
+            this.cbKategori.Size = new System.Drawing.Size(153, 21);
+            this.cbKategori.TabIndex = 11;
+            this.cbKategori.Text = "Bİr Değer Seçiniz";
+            // 
+            // cbMarketAdi
+            // 
+            this.cbMarketAdi.FormattingEnabled = true;
+            this.cbMarketAdi.Location = new System.Drawing.Point(711, 235);
+            this.cbMarketAdi.Name = "cbMarketAdi";
+            this.cbMarketAdi.Size = new System.Drawing.Size(153, 21);
+            this.cbMarketAdi.TabIndex = 12;
+            this.cbMarketAdi.Text = "Bİr Değer Seçiniz";
+            // 
+            // dtTarih
+            // 
+            this.dtTarih.Location = new System.Drawing.Point(711, 195);
+            this.dtTarih.Name = "dtTarih";
+            this.dtTarih.Size = new System.Drawing.Size(153, 20);
+            this.dtTarih.TabIndex = 13;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(637, 302);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(88, 60);
+            this.btnKaydet.TabIndex = 14;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(776, 302);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(88, 60);
+            this.btnGuncelle.TabIndex = 15;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(312, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 24);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Marketler";
+            // 
+            // cbMarketListeleme
+            // 
+            this.cbMarketListeleme.FormattingEnabled = true;
+            this.cbMarketListeleme.Location = new System.Drawing.Point(418, 48);
+            this.cbMarketListeleme.Name = "cbMarketListeleme";
+            this.cbMarketListeleme.Size = new System.Drawing.Size(182, 21);
+            this.cbMarketListeleme.TabIndex = 16;
+            this.cbMarketListeleme.Text = "Bİr Değer Seçiniz";
+            this.cbMarketListeleme.SelectionChangeCommitted += new System.EventHandler(this.cbMarketListeleme_SelectionChangeCommitted);
+            // 
+            // contextBtn
+            // 
+            this.contextBtn.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.silToolStripMenuItem,
+            this.güncelleToolStripMenuItem});
+            this.contextBtn.Name = "contextBtn";
+            this.contextBtn.Size = new System.Drawing.Size(121, 48);
+            // 
+            // silToolStripMenuItem
+            // 
+            this.silToolStripMenuItem.Name = "silToolStripMenuItem";
+            this.silToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.silToolStripMenuItem.Text = "Sil";
+            this.silToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
+            // 
+            // güncelleToolStripMenuItem
+            // 
+            this.güncelleToolStripMenuItem.Name = "güncelleToolStripMenuItem";
+            this.güncelleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.güncelleToolStripMenuItem.Text = "Güncelle";
+            this.güncelleToolStripMenuItem.Click += new System.EventHandler(this.güncelleToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 553);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbMarketListeleme);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.dtTarih);
+            this.Controls.Add(this.cbMarketAdi);
+            this.Controls.Add(this.cbKategori);
+            this.Controls.Add(this.tbBirimFiyat);
+            this.Controls.Add(this.tbUrunAdi);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lwListeleme);
+            this.Controls.Add(this.cbKategoriListele);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextBtn.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbKategoriListele;
+        private System.Windows.Forms.ListView lwListeleme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbUrunAdi;
+        private System.Windows.Forms.TextBox tbBirimFiyat;
+        private System.Windows.Forms.ComboBox cbKategori;
+        private System.Windows.Forms.ComboBox cbMarketAdi;
+        private System.Windows.Forms.DateTimePicker dtTarih;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbMarketListeleme;
+        private System.Windows.Forms.ContextMenuStrip contextBtn;
+        private System.Windows.Forms.ToolStripMenuItem silToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem güncelleToolStripMenuItem;
+    }
+}
+
---
+++ StokTakip/StokTakip/Form1.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StokTakip
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection cnn = new SqlConnection("Data Source=DIMEC-DESKTOP;Initial Catalog=StokTakip;Integrated Security=True");
+        SqlDataReader dr;
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lwListeleme.Columns.Add("Urun ID", 50);
+            lwListeleme.Columns.Add("Urun Adi", 100);
+            lwListeleme.Columns.Add("Birim Fiyat", 100);
+            lwListeleme.Columns.Add("Tarih", 120);
+            lwListeleme.Columns.Add("Kategori Adı", 110);
+            lwListeleme.Columns.Add("Market Adı", 110);
+
+            cbDoldur();
+            //Goster();
+
+        }
+
+        public void cbDoldur()
+        {
+
+            //Kategori
+            List<KeyValuePair<int, string>> dataKategori = new List<KeyValuePair<int, string>>();
+            cnn.Open();
+            cmd = cnn.CreateCommand();
+            cmd.CommandText = "select * from tbl_Kategoriler";
+            SqlDataReader drKategori = cmd.ExecuteReader();
+            while (drKategori.Read())
+            {
+                dataKategori.Add(new KeyValuePair<int, string>(Convert.ToInt32(drKategori[0]), drKategori[1].ToString()));
+                //cbKategori.Items.Add(drKategori[1].ToString());
+            }
+            cbKategori.DataSource = new BindingSource(dataKategori, null);
+            cbKategori.DisplayMember = "Value";
+            cbKategori.ValueMember = "Key";
+            cnn.Close();
+            //dataKategori.Close();
+
+
+            //Marketler
+            List<KeyValuePair<int, string>> dataMarket = new List<KeyValuePair<int, string>>();
+            cnn.Open();
+            cmd = cnn.CreateCommand();
+            cmd.CommandText = "select * from tbl_Marketler";
+            SqlDataReader drMarketler = cmd.ExecuteReader();
+            while (drMarketler.Read())
+            {
+                dataMarket.Add(new KeyValuePair<int, string>(Convert.ToInt32(drMarketler[0]), drMarketler[1].ToString()));
+                //cbMarketAdi.Items.Add(drMarketler[1].ToString());
+            }
+            cbMarketAdi.DataSource = new BindingSource(dataMarket, null);
+            cbMarketAdi.DisplayMember = "Value";
+            cbMarketAdi.ValueMember = "Key";
+            cnn.Close();
+
+
+
+            //KategoriListeleme
+            cbKategoriListele.Items.Add("Boş Değer");
+            cnn.Open();
+            cmd = cnn.CreateCommand();
+            cmd.CommandText = "select * from tbl_Kategoriler";
+            SqlDataReader drKategoriListeleme = cmd.ExecuteReader();
+            while (drKategoriListeleme.Read())
+            {
+                cbKategoriListele.Items.Add(drKategoriListeleme[1].ToString());
+            }
+            cnn.Close();
+
+
+
+            //MarketlerListeleme
+            cbMarketListeleme.Items.Add("Boş Değer");
+            cnn.Open();
+            cmd = cnn.CreateCommand();
+            cmd.CommandText = "select * from tbl_Marketler";
+            SqlDataReader drMarketListeleme = cmd.ExecuteReader();
+            while (drMarketListeleme.Read())
+            {
+                cbMarketListeleme.Items.Add(drMarketListeleme[1].ToString());
+            }
+            cnn.Close();
+
+
+
+        }
+
+        public void Goster()
+        {
+            lwListeleme.Items.Clear();
+            if (cbMarketListeleme.SelectedIndex == -1 || cbMarketListeleme.SelectedIndex == 0)
+            {
+                sp_KategoriGoreUrunListeleme();
+            }
+            else if (cbKategoriListele.SelectedIndex == -1 || cbKategoriListele.SelectedIndex == 0)
+            {
+                sp_MarketGoreUrunListeleme();
+            }
+            else
+            {
+                sp_MarketveKategoriGoreUrunListeleme();
+            }
+
+            if (cbKategoriListele.SelectedIndex == -1 || cbKategoriListele.SelectedIndex == 0)
+            {
+                sp_MarketGoreUrunListeleme();
+            }
+            else if (cbMarketListeleme.SelectedIndex == -1 || cbMarketListeleme.SelectedIndex == 0)
+            {
+                sp_KategoriGoreUrunListeleme();
+            }
+            else
+            {
+                sp_MarketveKategoriGoreUrunListeleme();
+            }
+
+        }
+
+
+        public void sp_KategoriGoreUrunListeleme()
+        {
+            cnn.Open();
+
+            cmd = cnn.CreateCommand();
+            cmd.CommandText = "sp_KategoriGoreUrunListeleme";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@KategoriID", cbKategoriListele.SelectedIndex);
+
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                ListViewItem item = new ListViewItem(dr["UrunID"].ToString());
+                item.SubItems.Add(dr["UrunAdi"].ToString());
+                item.SubItems.Add(dr["BirimFiyat"].ToString());
+                item.SubItems.Add(dr["Tarih"].ToString());
+                item.SubItems.Add(dr["KategoriAdi"].ToString());
+                item.SubItems.Add(dr["MarketAdi"].ToString());
+
+                lwListeleme.Items.Add(item);
+            }
+            cnn.Close();
+        }
+
+        public void sp_MarketveKategoriGoreUrunListeleme()
+        {
+            cnn.Open();
+
+            cmd = cnn.CreateCommand();
+            cmd.CommandText = "sp_MarketveKategoriGoreUrunListeleme";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@KategoriID", cbKategoriListele.SelectedIndex);
+            cmd.Parameters.AddWithValue("@MarketID", cbMarketListeleme.SelectedIndex);
+
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                ListViewItem item = new ListViewItem(dr["UrunID"].ToString());
+                item.SubItems.Add(dr["UrunAdi"].ToString());
+                item.SubItems.Add(dr["BirimFiyat"].ToString());
+                item.SubItems.Add(dr["Tarih"].ToString());
+                item.SubItems.Add(dr["KategoriAdi"].ToString());
+                item.SubItems.Add(dr["MarketAdi"].ToString());
+
+                lwListeleme.Items.Add(item);
+            }
+            cnn.Close();
+        }
+
+        public void sp_MarketGoreUrunListeleme()
+        {
+            cnn.Open();
+
+            cmd = cnn.CreateCommand();
+            cmd.CommandText = "sp_MarketGoreUrunListeleme";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@MarketID", cbMarketListeleme.SelectedIndex);
+
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                ListViewItem item = new ListViewItem(dr["UrunID"].ToString());
+                item.SubItems.Add(dr["UrunAdi"].ToString());
+                item.SubItems.Add(dr["BirimFiyat"].ToString());
+                item.SubItems.Add(dr["Tarih"].ToString());
+                item.SubItems.Add(dr["KategoriAdi"].ToString());
+                item.SubItems.Add(dr["MarketAdi"].ToString());
+
+                lwListeleme.Items.Add(item);
+            }
+            cnn.Close();
+        }
+
+        private void cbKategoriListele_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            lwListeleme.Items.Clear();
+            if(cbMarketListeleme.SelectedIndex== -1 || cbMarketListeleme.SelectedIndex==0)
+            {
+                sp_KategoriGoreUrunListeleme();
+            }
+            else if(cbKategoriListele.SelectedIndex == -1 || cbKategoriListele.SelectedIndex == 0)
+            {
+                sp_MarketGoreUrunListeleme();
+            }
+            else
+            {
+                sp_MarketveKategoriGoreUrunListeleme();
+            }
+
+
+            
+
+            
+        }
+        private void cbMarketListeleme_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            lwListeleme.Items.Clear();
+            if (cbKategoriListele.SelectedIndex == -1 || cbKategoriListele.SelectedIndex == 0)
+            {
+                sp_MarketGoreUrunListeleme();
+            }
+            else if (cbMarketListeleme.SelectedIndex == -1 || cbMarketListeleme.SelectedIndex == 0)
+            {
+                sp_KategoriGoreUrunListeleme();
+            }
+            else
+            {
+                sp_MarketveKategoriGoreUrunListeleme();
+            }
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            KeyValuePair<int, string> selectedPairKategori = (KeyValuePair<int, string>)cbKategori.SelectedItem;
+            KeyValuePair<int, string> selectedPairMarket = (KeyValuePair<int, string>)cbMarketAdi.SelectedItem;
+            cnn.Open();
+
+            cmd = cnn.CreateCommand();
+            cmd.CommandText = "sp_UrunlerINSERT";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@KategoriID", selectedPairKategori.Key);
+            cmd.Parameters.AddWithValue("@MarketID", selectedPairMarket.Key);
+            cmd.Parameters.AddWithValue("@UrunAdi", tbUrunAdi.Text);
+            cmd.Parameters.AddWithValue("@BirimFiyat", tbBirimFiyat.Text);
+            cmd.Parameters.AddWithValue("@Tarih", dtTarih.Value);
+
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            KeyValuePair<int, string> selectedPairKategori = (KeyValuePair<int, string>)cbKategori.SelectedItem;
+            KeyValuePair<int, string> selectedPairMarket = (KeyValuePair<int, string>)cbMarketAdi.SelectedItem;
+            cnn.Open();
+            
+            cmd = cnn.CreateCommand();
+            cmd.CommandText = "sp_UrunlerUPDATE";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@UrunID", GuncellemeID);
+            cmd.Parameters.AddWithValue("@UrunAdi", tbUrunAdi.Text);
+            cmd.Parameters.AddWithValue("@BirimFiyat", tbBirimFiyat.Text);
+            cmd.Parameters.AddWithValue("@Tarih", dtTarih.Value);
+            cmd.Parameters.AddWithValue("@KategoriID", selectedPairKategori.Key);
+            cmd.Parameters.AddWithValue("@MarketID", selectedPairMarket.Key);
+
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+            //Goster();
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem eachItem in lwListeleme.SelectedItems)
+            {
+                int seciliindex = Convert.ToInt32(eachItem.SubItems[0].Text);
+                cnn.Open();
+
+                cmd = cnn.CreateCommand();
+                cmd.CommandText = "sp_UrunlerDELETE";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@UrunID", seciliindex);
+
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+                
+
+                lwListeleme.Items.Remove(eachItem);
+            }
+        }
+
+        int GuncellemeID;
+        private void güncelleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            
+
+            foreach (ListViewItem eachItem in lwListeleme.SelectedItems)
+            {
+                GuncellemeID=Convert.ToInt32(eachItem.SubItems[0].Text);
+                cnn.Open();
+                
+                cmd = cnn.CreateCommand();
+                cmd.CommandText = "sp_UrunGuncelleKategoriMarketID";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+
+                cmd.Parameters.AddWithValue("@UrunID", eachItem.SubItems[0].Text);
+
+                SqlDataReader drUrunGuncelle = cmd.ExecuteReader();
+                while (drUrunGuncelle.Read())
+                {
+                    cbKategori.SelectedIndex = Convert.ToInt32(drUrunGuncelle[0])-1;
+                    cbMarketAdi.SelectedIndex = Convert.ToInt32(drUrunGuncelle[1])-1;
+                }
+                cnn.Close();
+
+                tbUrunAdi.Text = eachItem.SubItems[1].Text;
+                tbBirimFiyat.Text = eachItem.SubItems[2].Text;
+                dtTarih.Value = Convert.ToDateTime(eachItem.SubItems[3].Text);
+
+
+
+
+                // urun ıd sinden kategori ve market ıd si al
+
+
+            }
+        }
+    }
+}
